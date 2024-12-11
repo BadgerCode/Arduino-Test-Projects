9fab68fe65e60b51b1dd49405157c3813ca68d12
Michael Hawkins <BadgerCode@users.noreply.github.com>
2024-12-11 22:32:03 +0000

    More LED strip stuff

--- 08_led_strip/08_led_strip.ino
+++ 08_led_strip/08_led_strip.ino
@@ -1,27 +1,68 @@
 #include <FastLED.h>
 
 #define DATA_PIN 7
-#define NUM_LEDS 5
+#define NUM_LEDS 15
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  // FastLED.setBrightness(255);
 }
 
+
+
+// 0. Single colour
 void loop() {
-  // Set all LEDs to red
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
-  FastLED.show();
-  delay(1000);
-
-  // Set all LEDs to green
-  fill_solid(leds, NUM_LEDS, CRGB::Green);
-  FastLED.show();
-  delay(1000);
-
   // Set all LEDs to blue
-  fill_solid(leds, NUM_LEDS, CRGB::Blue);
+  fill_solid(leds, NUM_LEDS, CRGB(0, 0, 255));
   FastLED.show();
-  delay(1000);
+  delay(5000);
 }
+
+
+
+// 1. Simple alternating colours loop
+// void loop() {
+//   // Set all LEDs to red
+//   fill_solid(leds, NUM_LEDS, CRGB::Red);
+//   FastLED.show();
+//   delay(5000);
+
+//   // Set all LEDs to green
+//   fill_solid(leds, NUM_LEDS, CRGB::Green);
+//   FastLED.show();
+//   delay(5000);
+
+//   // Set all LEDs to blue
+//   fill_solid(leds, NUM_LEDS, CRGB::Blue);
+//   FastLED.show();
+//   delay(5000);
+
+//   fill_solid(leds, NUM_LEDS, CRGB::White);
+//   FastLED.show();
+//   delay(5000);
+// }
+
+
+
+// 2. Simple RGB hue rotation (all same colour)
+// uint8_t hue = 0;
+// void loop() {
+//   fill_solid(leds, NUM_LEDS, CHSV(hue++, 255, 255));
+//   FastLED.show();
+//   delay(20);
+// }
+
+
+// 3. Spinning hue rotation
+// uint8_t hue = 0;
+// void loop() {
+//   for (int i = 0; i < NUM_LEDS; i++) {
+//     leds[i] = CHSV(hue, 255, 255);
+//     hue += 15;
+//   }
+
+//   FastLED.show();
+//   delay(40);
+// }
